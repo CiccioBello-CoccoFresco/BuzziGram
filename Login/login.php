f96c0745b989b488643f7d3718173d2d6c52ebbb
RafFra1101 <raffaele.mentina@gmail.com>
2020-05-19 17:20:31 +0200

    fix login

--- Login/login.php
+++ Login/login.php
@@ -11,10 +11,11 @@ $email = $_POST['email'];
 $psw = md5($_POST['psw']);
 
 $conn = openConn();
-
-$sql = $conn->prepare("Select id from utente where email = ? and password = ?");
-$sql->bind_param('ss', $email, $psw);
-$result = $sql->execute();
+$sql = "Select id from utente where email = ? and password = ?";
+$stmt = $conn->prepare($sql); 
+$stmt->bind_param("ss", $email, $psw);
+$stmt->execute();
+$result = $stmt->get_result();
 closeConn($conn);
 if($result->num_rows != 0) echo 'Benvenuto';
 else echo 'Credenziali errate';
