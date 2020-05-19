343686b5201de2b9bcca2e48fca32321b29d8b83
RafFra1101 <raffaele.mentina@gmail.com>
2020-05-19 17:11:41 +0200

    Inizio login.php

---
+++ Login/login.php
@@ -0,0 +1,22 @@
+<head>
+    <?php
+        include_once '../utils/dbConnection.php';
+    ?>
+</head>
+<?php
+if(!isset($_POST['email'])||!isset($_POST['psw'])){
+    Header('Location: login.html');
+}
+$email = $_POST['email'];
+$psw = md5($_POST['psw']);
+
+$conn = openConn();
+
+$sql = $conn->prepare("Select id from utente where email = ? and password = ?");
+$sql->bind_param('ss', $email, $psw);
+$result = $sql->execute();
+closeConn($conn)
+if($result->num_rows != 0) echo 'Benvenuto';
+else echo 'Credenziali errate';
+
+?>
