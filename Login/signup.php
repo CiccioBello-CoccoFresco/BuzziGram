0f57a6d5efa0185222ec39a51e44a94ee98e76eb
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2020-05-19 22:05:14 +0200

    Puffo gay

--- Login/signup.php
+++ Login/signup.php
@@ -16,8 +16,7 @@
         $email = $_POST['email'];
         $psw = md5($_POST['psw']);
         $as = $_POST['as'];
-        if($_POST['rap']) $rap = 1;
-        else $rap = 0;
+        $_POST['rap'] === '1' ? $rap = 1 : $rap = 0;
         $conn = openConn();
         $conn->begin_transaction();
         $stmt = $conn->prepare('insert into studente(nome, cognome) values (?,?)');
@@ -25,24 +24,25 @@
         $stmt->execute();
         $idStudente = $stmt->insert_id;
         $stmt = $conn->prepare('insert into utente(id, email, password) values (?,?,?)');
-        $stmt->bind_param("iss", $id, $email, $password);
+        $stmt->bind_param("iss", $idStudente, $email, $psw);
         $stmt->execute();
         $stmt = $conn->prepare('select id from classe where anno = ? and sezione = ? and anno_scolastico = ?');
         $stmt->bind_param("iss", $anno, $sezione, $as);
         $stmt->execute();
-        $idClasse = $stmt->get_result();
+        $idClasse = $stmt->get_result()->fetch_row()[0];
         $stmt = $conn->prepare('insert into frequenta values (?,?,?)');
-        $stmt->bind_param("isi", $idStudente, $idClasse, $rap);
+        $stmt->bind_param("iii", $idStudente, $idClasse, $rap);
         $stmt->execute();
         $app = $conn->commit();
         closeConn($conn);
         if($app){
             echo '<script> alert("Registrazione avvenuta con successo. Stai per essere reindirizzato alla pagina di login"); 
-            window.location = "login.html";
-            </script>';
+            '// window.location = "login.html";
+            .'</script>';
         } else {
-            echo 'Problema di registrazione, la preghiamo di riprovare!';
-            Header("Location: ".$_SERVER['HTTP_REFERER']);
+            echo '<script> alert("Problema di registrazione, la preghiamo di riprovare!"); 
+            window.location = "#";
+            </script>';
         }
     }
 ?>
--- js/index.js
+++
@@ -1,5 +0,0 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-
-console.log("$="+ $);
