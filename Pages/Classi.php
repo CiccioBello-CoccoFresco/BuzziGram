cf9d3175489b3a6c36821f7b7995a0078b09506d
Manlel <0manunam080@gmail.com>
2020-05-19 19:56:13 +0200

    Classi+alunni

---
+++ Pages/Classi.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Classi</title>
+    </head>
+
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <th>Biennio</th>
+                </tr>
+                <?php ?>
+                <tr>
+                    
+                </tr>
+                <?php ?>
+            </table>
+        </div>
+        <div>
+        <table>
+                <tr>
+                    <th>Triennio</th>
+                </tr>
+                <tr>
+
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
