0ff62a57310001f000b4ee483373cf41997b7941
RafFra1101 <raffaele.mentina@gmail.com>
2020-05-21 21:43:09 +0200

    caricamento "più rapido" delle foto e loading gif

--- js/alunni.js
+++ js/alunni.js
@@ -17,40 +17,14 @@ function caricaAlunni(classe){
         for(var i=0; i<data.length; i++){
             var idStud = data[i]['studente'];
             var cognomeNomeStud = data[i]['cognome'] +" "+data[i]['nome'];
-            stringa = stringa + "<div class='gallery'> <div class='desc'>"+cognomeNomeStud+"</div><a target='_blank'><img id ='f"+idStud+"' class = 'foto'></a> <div id = 'd"+idStud+"'class='desc'></div></div>";
-            caricaFoto(classe, idStud); 
+            var pathImg = data[i]['file'];
+            var frase = data[i]['frase'];
+            stringa = stringa + "<div class='gallery'> <div class='desc'>"+cognomeNomeStud+"</div><a target='_blank'><img src='"+pathImg+"'class ='foto'></a> <div class='desc'>"+frase+"</div></div>";
         }
         $("#container").append(stringa);
+        $("#loading").hide();
 
 
-    })
-    .fail(function () {
-        console.log("errore");
-    })
-}
-
-function caricaFoto(classe, studente){//ritorna la stringa da inserire in img src di uno studente di una classe
-    var obj = {
-        classe    : classe,
-        studente  : studente
-    };
-    var url = "../php/getFoto.php?" + $.param(obj);
-    $.ajax({
-        url: url
-        ,dataType: "json"
-        ,error: function(XMLHttpRequest, textStatus, errorThrown) {
-            console.log(textStatus);
-        }
-    })
-    .done(function (data){
-        if(data[0] === "norows"){
-            $("#f"+studente).attr("src", "../img/Cicciobello.png");
-            $("#d"+studente).text("Foto non inserita");
-        }else{
-            $("#f"+studente).attr("src", data[0]);
-            $("#d"+studente).text(data[1]);
-        }
-        
     })
     .fail(function () {
         console.log("errore");
--- php/getAlunni.php
+++ php/getAlunni.php
@@ -3,7 +3,10 @@
     if(isset($_GET['classe'])){
         $classe = $_GET["classe"];
         
-        $sql = 'SELECT studente, cognome, nome FROM frequenta f join studente s on f.studente = s.matricola WHERE classe ="'. $classe . '"order by cognome asc';
+        $sql = 'SELECT f.studente, cognome, nome, file, frase
+        FROM frequenta f join studente s on f.studente = s.matricola left join immagine i on s.matricola = i.studente
+        WHERE f.classe = '.$classe.' 
+        order by cognome asc';
         $conn = openConn();
         $result = $conn->query($sql);
         closeConn($conn);
@@ -11,6 +14,14 @@
         $array = array();
         if($result->num_rows === 0) array_push($array,"norows");;
         while($row = $result->fetch_assoc()) {
+            if(isset($row['file'])) {
+                $pathSrc = 'data:image/jpeg;base64,'.base64_encode( $row['file'] );
+            }else{
+                $pathSrc = "../img/Cicciobello.png";
+                $row['frase'] = "Foto non inserita";
+            }
+            $row['file'] = $pathSrc;
+            
             array_push($array,$row);
         }
 
