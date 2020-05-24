c967a7e0229b8bf4b52f6e39b9f201de108434a9
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2020-05-24 10:45:04 +0200

    image fits with the power of css

--- js/registrazione.js
+++ js/registrazione.js
@@ -6,7 +6,7 @@ require('./registrazione_mdc');
 import classi from "./classiLib";
 import img  from "../img/polaroid.jpg";
 
-$("#imgbox").append('<img src="./assets/'+img+'">').addClass('mdc-image-list__image').addClass('center-fit');
+// $("#imgbox").append('<img class="center-fit" src="./assets/'+img+'">');
 classi.caricaClassi();
 
 $('#sub').on('click', ()=>{
