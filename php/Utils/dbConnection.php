095f6c1ac8e4c97bf8888fe1ee0b2776689a72fb
RafFra1101 <raffaele.mentina@gmail.com>
2020-05-25 08:15:19 +0200

    ngrok db

--- php/Utils/dbConnection.php
+++ php/Utils/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 	function openConn(){
-        $servername = "0.tcp.ngrok.io:15891";
+        $servername = "0.tcp.ngrok.io:17204";
         $username = "root";
         $password = "";
 		$db = "buzzigram";
